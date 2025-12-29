c6f546e7b7b3e7b6db2119d62a7bb0bc955f9fd9
ruipeng <1041589370@qq.com>
2025-12-29 15:10:31 +0800

    新增对齐摄像头的两个页面

--- screen.py
+++ screen.py
@@ -280,6 +280,7 @@ class KlipperScreen(Gtk.Window):
                 "exclude_object": ["current_object", "objects", "excluded_objects"],
                 "manual_probe": ['is_active'],
                 "screws_tilt_adjust": ['results', 'error'],
+                "ktamv": [],
                 "stepper_enable": ['steppers'],
                 "save_variables": ['variables'],
             }
