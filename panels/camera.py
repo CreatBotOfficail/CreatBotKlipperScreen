675ee5bba1b05e45e5208b716beed4700f5ae73b
zkk <1007518571@qq.com>
2026-01-13 14:38:37 +0800

    适配buildroot，使用gst库替换mpv播放方式

--- panels/camera.py
+++ panels/camera.py
@@ -1,4 +1,4 @@
-import mpv
+
 import logging
 import gi
 
@@ -82,50 +82,147 @@ class Panel(ScreenPanel):
         if self.mpv:
             self.mpv.terminate()
 
-        self.mpv = mpv.MPV(
-            fullscreen=True,
-            log_handler=self.log,
-            hwdec='no',
-            video_sync='display-desync',
-            framedrop='decoder+vo',
-            profile='low-latency',
-            cache=False,
-            demuxer_max_bytes='8K',
-            demuxer_max_back_bytes='16K',
-            demuxer_readahead_secs=0,
-            stream_buffer_size='8K',
-            correct_pts=False,
-            demuxer_thread=False,
-            network_timeout=0.5,
-            stream_lavf_o='flags=low_delay',
-            save_position_on_quit=False,
-            keep_open=False
-        )
+        try:
+            gi.require_version('Gst', '1.0')
+            gi.require_version('GstVideo', '1.0')
+        except ValueError:
+            pass
+        from gi.repository import Gst, Gdk, GLib, Pango
 
-        self.mpv.vf = vf
+        if not Gst.is_initialized():
+            Gst.init(None)
 
-        with suppress(Exception):
-            self.mpv.profile = 'sw-fast'
+        class VideoWidget(Gtk.DrawingArea):
+            def __init__(self):
+                super().__init__()
+                self.connect("draw", self.on_draw)
+                self.pixbuf = None
 
-        # LOW LATENCY PLAYBACK
-        with suppress(Exception):
-            self.mpv.profile = 'low-latency'
-        self.mpv.untimed = True
-        self.mpv.audio = 'no'
+            def on_draw(self, widget, cr):
+                if self.pixbuf:
+                    try:
+                        Gdk.cairo_set_source_pixbuf(cr, self.pixbuf, 0, 0)
+                        cr.paint()
+                    except Exception:
+                        pass
+                return False
 
-        @self.mpv.on_key_press('MBTN_LEFT' or 'MBTN_LEFT_DBL')
-        def clicked():
-            self.mpv.quit(0)
+            def set_pixbuf(self, pixbuf):
+                self.pixbuf = pixbuf
+                self.queue_draw()
 
-        logging.debug(f"Camera URL: {url}")
-        self.mpv.play(url)
+        class GstPlayer:
+            def __init__(self_inner):
+                self_inner.pipeline = None
+                self_inner.window = None
+                self_inner.loop = None
+                self_inner.video_widget = None
+
+            def terminate(self_inner):
+                if self_inner.pipeline:
+                    self_inner.pipeline.set_state(Gst.State.NULL)
+                if self_inner.window:
+                    with suppress(Exception):
+                        self_inner.window.destroy()
+                if self_inner.loop and self_inner.loop.is_running():
+                    self_inner.loop.quit()
+
+        self.mpv = GstPlayer()
+        self.mpv.video_widget = VideoWidget()
+        
+        screen = Gdk.Screen.get_default()
+        width = screen.get_width() if screen else 1024
+        height = screen.get_height() if screen else 600
+        
+        pipeline_parts = []
+        if url.startswith("rtsp"):
+            # Hardware accelerated pipeline for RTSP as requested
+            pipeline_parts.append(f"rtspsrc location={url} latency=0")
+            pipeline_parts.append("rtph264depay")
+            pipeline_parts.append("h264parse")
+            pipeline_parts.append("mppvideodec")
+            pipeline_parts.append("videoconvert")
+            pipeline_parts.append("video/x-raw,format=RGB")
+        else:
+            # Generic fallback
+            pipeline_parts.append(f"urisourcebin uri={url}")
+            pipeline_parts.append("decodebin")
+            pipeline_parts.append("videoconvert")
+
+        if cam["flip_horizontal"]:
+            pipeline_parts.append("videoflip method=horizontal-flip")
+        if cam["flip_vertical"]:
+            pipeline_parts.append("videoflip method=vertical-flip")
+
+        rot = cam.get('rotation', 0)
+        if rot == 90:
+            pipeline_parts.append("videoflip method=clockwise")
+        elif rot == 180:
+            pipeline_parts.append("videoflip method=rotate-180")
+        elif rot == 270:
+            pipeline_parts.append("videoflip method=counterclockwise")
+
+        # Force format and scale
+        pipeline_parts.append(f"videoscale ! video/x-raw,width={width},height={height}")
+        pipeline_parts.append("gdkpixbufsink name=sink")
 
         try:
-            self.mpv.wait_for_playback()
-        except mpv.ShutdownError:
-            logging.info('Exiting Fullscreen')
+            launch_str = " ! ".join(pipeline_parts)
+            logging.info(f"Gst launch: {launch_str}")
+            self.mpv.pipeline = Gst.parse_launch(launch_str)
         except Exception as e:
-            logging.exception(e)
+            logging.exception("Failed to launch gst")
+            self._screen.show_popup_message(f"Gst Error: {e}")
+            self.mpv = None
+            return
+
+        self.mpv.window = Gtk.Window()
+        self.mpv.window.fullscreen()
+        
+        # UX Improvement: Black background + Spinner
+        self.mpv.window.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 0, 1))
+        
+        overlay = Gtk.Overlay()
+        overlay.add(self.mpv.video_widget)
+        
+        spinner = Gtk.Label()
+        spinner.set_markup(f"<span color='white' font_size='large'>{_('Loading...')}</span>")
+        spinner.set_halign(Gtk.Align.CENTER)
+        spinner.set_valign(Gtk.Align.CENTER)
+        
+        overlay.add_overlay(spinner)
+        
+        self.mpv.window.add(overlay)
+
+        def on_new_pixbuf(sink, pspec):
+            pixbuf = sink.get_property(pspec.name)
+            if pixbuf and self.mpv and self.mpv.video_widget:
+                GLib.idle_add(self.mpv.video_widget.set_pixbuf, pixbuf)
+                GLib.idle_add(spinner.hide) 
+
+        sink = self.mpv.pipeline.get_by_name("sink")
+        sink.connect("notify::last-pixbuf", on_new_pixbuf)
+
+        def quit_player(*args):
+             self.mpv.terminate()
+
+        self.mpv.window.connect("button-press-event", quit_player)
+        self.mpv.window.connect("destroy", quit_player)
+
+        bus = self.mpv.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message::eos", quit_player)
+        bus.connect("message::error", quit_player)
+
+        self.mpv.window.show_all()
+        self.mpv.pipeline.set_state(Gst.State.PLAYING)
+
+        self.mpv.loop = GLib.MainLoop()
+        try:
+            self.mpv.loop.run()
+        except:
+            pass
+
         self.mpv.terminate()
         self.mpv = None
         if len(self._printer.cameras) == 1:
