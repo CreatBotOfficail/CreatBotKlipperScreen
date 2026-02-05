cc6f4373bbfee2ce9c16b6761e5b3b90147e86de
ruipeng <1041589370@qq.com>
2026-02-05 11:05:12 +0800

    重构共振补偿页面

--- panels/input_shaper.py
+++ panels/input_shaper.py
@@ -20,19 +20,16 @@ class Panel(ScreenPanel):
         super().__init__(screen, title)
         self.freq_xy_adj = {}
         self.shaper_buttons = {}
-        self.calibrate_btn = self._gtk.Button("move", _('Finding ADXL'), "color1", lines=1)
-        self.calibrate_btn.connect("clicked", self.on_popover_clicked)
-        self.calibrate_btn.set_sensitive(False)
-        self.status = Gtk.Label(hexpand=True, vexpand=False, halign=Gtk.Align.START, ellipsize=Pango.EllipsizeMode.END)
+        self.status = Gtk.Label(hexpand=True, vexpand=True, halign=Gtk.Align.START, ellipsize=Pango.EllipsizeMode.END)
         self.calibrating_axis = None
         self.calibrating_axis = None
 
-        auto_calibration_label = Gtk.Label(hexpand=True)
-        auto_calibration_label.set_markup("<big><b>{}</b></big>".format(_("Auto Calibration")))
-
-        auto_grid = Gtk.Grid()
-        auto_grid.attach(auto_calibration_label, 0, 0, 1, 1)
-        auto_grid.attach(self.calibrate_btn, 1, 0, 1, 1)
+        self.measure_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+        
+        self.adxl_status_label = Gtk.Label(label=_('Finding ADXL'))
+        self.adxl_status_label.set_hexpand(True)
+        self.adxl_status_label.set_vexpand(True)
+        self.measure_box.pack_start(self.adxl_status_label, False, False, 0)
 
         input_grid = Gtk.Grid()
 
@@ -43,7 +40,7 @@ class Panel(ScreenPanel):
 
             self.freq_xy_adj[dim_freq['config']] = Gtk.Adjustment(0, dim_freq['min'], dim_freq['max'], 0.1)
             scale = Gtk.Scale(adjustment=self.freq_xy_adj[dim_freq['config']],
-                              digits=1, hexpand=True, valign=Gtk.Align.END, has_origin=True)
+                              digits=1, hexpand=True, valign=Gtk.Align.CENTER, has_origin=True)
             scale.get_style_context().add_class("option_slider")
             scale.connect("button-release-event", self.set_opt_value, dim_freq['config'])
 
@@ -58,34 +55,20 @@ class Panel(ScreenPanel):
             self.shaper_buttons[shaper_slug] = menu_btn
 
             combo_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
-            combo_box.pack_start(menu_btn, True, True, 0)
+            combo_box.pack_start(menu_btn, False, False, 0)
             arrow = Gtk.Arrow(arrow_type=Gtk.ArrowType.DOWN, shadow_type=Gtk.ShadowType.NONE)
             combo_box.pack_start(arrow, False, False, 0)
 
-            input_grid.attach(axis_lbl, 0, i + 2, 1, 1)
-            input_grid.attach(scale, 1, i + 2, 1, 1)
-            input_grid.attach(combo_box, 2, i + 2, 1, 1)
+            input_grid.attach(axis_lbl, 0, i, 1, 1)
+            input_grid.attach(scale, 1, i, 1, 1)
+            input_grid.attach(combo_box, 2, i, 1, 1)
 
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        box.add(auto_grid)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        box.add(self.measure_box)
         box.add(input_grid)
         box.add(self.status)
 
         self.content.add(box)
-
-        pobox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        test_x = self._gtk.Button(label=_("Measure X"))
-        test_x.connect("clicked", self.start_calibration, "x")
-        pobox.pack_start(test_x, True, True, 5)
-        test_y = self._gtk.Button(label=_("Measure Y"))
-        test_y.connect("clicked", self.start_calibration, "y")
-        pobox.pack_start(test_y, True, True, 5)
-        test_both = self._gtk.Button(label=_("Measure Both"))
-        test_both.connect("clicked", self.start_calibration, "both")
-        pobox.pack_start(test_both, True, True, 5)
-        self.labels['popover'] = Gtk.Popover()
-        self.labels['popover'].add(pobox)
-        self.labels['popover'].set_position(Gtk.PositionType.LEFT)
 
         self.shaper_popovers = {}
         for dim_freq in XY_FREQ:
@@ -131,22 +114,22 @@ class Panel(ScreenPanel):
             f'SHAPER_TYPE_Y={shaper_type_y}'
         )
 
-    def on_popover_clicked(self, widget):
-        self.labels['popover'].set_relative_to(widget)
-        self.labels['popover'].show_all()
 
     def start_calibration(self, widget, method):
-        self.labels['popover'].popdown()
         if self._printer.get_stat("toolhead", "homed_axes") != "xyz":
             self._screen._ws.klippy.gcode_script("G28")
         self.calibrating_axis = method
         if method == "x":
-            self._screen._send_action(self.calibrate_btn, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE AXIS=X'})
+            self._screen._send_action(widget, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE AXIS=X'})
         if method == "y":
-            self._screen._send_action(self.calibrate_btn, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE AXIS=Y'})
+            self._screen._send_action(widget, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE AXIS=Y'})
         if method == "both":
-            self._screen._send_action(self.calibrate_btn, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE'})
-        self.calibrate_btn.set_label(_('Calibrating') + '...')
+            self._screen._send_action(widget, "printer.gcode.script", {"script": 'SHAPER_CALIBRATE'})
+        
+        widget.set_label(_('Calibrating') + '...')
+        for child in self.measure_box.get_children():
+            if isinstance(child, Gtk.Button):
+                child.set_sensitive(False)
 
     def set_opt_value(self, widget, opt, *args):
         self.set_input_shaper()
@@ -175,14 +158,38 @@ class Panel(ScreenPanel):
         self.status.set_text(f"{data.replace('shaper_', '').replace('damping_', '')}")
         data = data.lower()
         if 'got 0' in data:
-            self.calibrate_btn.set_label(_('Check ADXL Wiring'))
-            self.calibrate_btn.set_sensitive(False)
+            for child in self.measure_box.get_children():
+                self.measure_box.remove(child)
+            error_label = Gtk.Label(label=_('Check ADXL Wiring'))
+            error_label.set_hexpand(True)
+            error_label.set_halign(Gtk.Align.CENTER)
+            self.measure_box.pack_start(error_label, True, True, 0)
+            self.measure_box.show_all()
         if 'unknown command:"accelerometer_query"' in data:
-            self.calibrate_btn.set_label(_('ADXL Not Configured'))
-            self.calibrate_btn.set_sensitive(False)
+            for child in self.measure_box.get_children():
+                self.measure_box.remove(child)
+            error_label = Gtk.Label(label=_('ADXL Not Configured'))
+            error_label.set_hexpand(True)
+            error_label.set_halign(Gtk.Align.CENTER)
+            self.measure_box.pack_start(error_label, True, True, 0)
+            self.measure_box.show_all()
         if 'adxl345 values' in data or 'axes noise' in data:
-            self.calibrate_btn.set_sensitive(True)
-            self.calibrate_btn.set_label(_('Auto-calibrate'))
+            for child in self.measure_box.get_children():
+                self.measure_box.remove(child)
+            
+            def create_measure_button(icon, label, callback_arg):
+                btn = self._gtk.Button(icon, label=label)
+                btn.connect('clicked', self.start_calibration, callback_arg)
+                btn.set_size_request(-1, self._gtk.content_height // 5)
+                btn.set_valign(Gtk.Align.CENTER)
+                self.measure_box.pack_start(btn, True, True, 5)
+                return btn
+
+            create_measure_button("measure_x", _('Measure X'), 'x')
+            create_measure_button("measure_y", _('Measure Y'), 'y')
+            create_measure_button("move", _('Measure Both'), 'both')
+            
+            self.measure_box.show_all()
         # Recommended shaper_type_y = ei, shaper_freq_y = 48.4 Hz
         if 'recommended shaper_type_' in data:
             results = re.search(r'shaper_type_(?P<axis>[xy])\s*=\s*(?P<shaper_type>.*?), shaper_freq_.\s*=\s*('
@@ -192,8 +199,13 @@ class Panel(ScreenPanel):
             self.freq_xy_adj['shaper_freq_' + results['axis']].set_value(float(results['shaper_freq']))
             self.shaper_buttons['shaper_type_' + results['axis']].set_label(results['shaper_type'])
             if self.calibrating_axis == results['axis'] or (self.calibrating_axis == "both" and results['axis'] == 'y'):
-                self.calibrate_btn.set_sensitive(True)
-                self.calibrate_btn.set_label(_('Calibrated'))
+                for child in self.measure_box.get_children():
+                    if isinstance(child, Gtk.Button):
+                        child.set_sensitive(True)
+                        if hasattr(child, "reset_label"):
+                            child.set_label(child.reset_label)
+                        elif 'Calibrating' in child.get_label():
+                            child.set_label(_('Measure ' + child.get_label().split()[-1]) if len(child.get_label().split()) > 1 else _('Measure X'))
                 self.save_config()
         # shaper_type_y:ei shaper_freq_y:48.400 damping_ratio_y:0.100000
         if 'shaper_type_' in data:
